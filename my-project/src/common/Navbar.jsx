c2c6958f8951fb634123e58469dd20703c848096
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-20 18:06:39 +0530

    responsive navbar updated

--- my-project/src/common/Navbar.jsx
+++ my-project/src/common/Navbar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { HiMenu, HiX } from "react-icons/hi";
 import Logo from "../assets/__-removebg-preview.png";
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [activeLink, setActiveLink] = useState("#home");
-  const navigate =useNavigate();
+  const navigate = useNavigate();
 
   const navLinks = [
     { href: "#home", label: "Home" },
@@ -55,15 +55,16 @@ const Navbar = () => {
           ))}
         </div>
 
-        
         <button className="hidden md:block bg-pink-600 text-white px-6 py-2.5 rounded-lg hover:bg-pink-700 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100">
-          <a href="#newsletter"><Link to="/login">Sign in</Link></a>
+          <a href="#newsletter">
+            <Link to="/login">Sign in</Link>
+          </a>
         </button>
       </div>
 
       {/* Mobile Menu */}
       {isMenuOpen && (
-        <div className="md:hidden bg-white border-t border-gray-100 py-4">
+        <div className="md:hidden  border-t border-gray-100 py-4">
           <div className="container mx-auto px-4 space-y-4">
             {navLinks.map((link, index) => (
               <a
@@ -83,8 +84,8 @@ const Navbar = () => {
                 {link.label}
               </a>
             ))}
-            <button className="w-full bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100">
-              Get in touch
+            <button className="w-full bg-pink-600 text-white px-6 py-2.5 rounded-lg hover:bg-pink-700 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100">
+              <Link to="/login">Sign in</Link>
             </button>
           </div>
         </div>
