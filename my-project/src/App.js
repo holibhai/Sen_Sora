878a3d839db8a7340fc593e62b1b9626f339ea61
holibhai <146805057+holibhai@users.noreply.github.com>
2025-03-20 18:00:28 +0530

    app.js updated

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -3,10 +3,12 @@ import Register from './auth/Register'
 import Login from "./auth/Login";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./common/Footer";
+import Navbar from "./common/Navbar";
 
 const App = () => {
   return (
-    <div className="bg-gradient-to-tr from-indigo-500/30 to-pink-500/30"> 
+    <div className="bg-gradient-to-tr from-indigo-500/30 to-pink-500/30  "> 
+    <Navbar/>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
