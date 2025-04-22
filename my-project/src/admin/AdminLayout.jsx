006d4b6ac11d0c61cda2cdcad4d5ca87094c9301
holibhai <146805057+holibhai@users.noreply.github.com>
2025-04-22 23:35:19 +0530

    changes

---
+++ my-project/src/admin/AdminLayout.jsx
@@ -0,0 +1,21 @@
+// src/layouts/AdminLayout.jsx
+import React from "react";
+import { Outlet } from "react-router-dom";
+import AdminHeader from "./AdminHeader";
+import AdminSidebar from "./AdminSidebar";
+const AdminLayout = () => {
+  return (
+    <div className="flex h-screen">
+      <AdminSidebar />
+      <div className="flex flex-col flex-1">
+        <AdminHeader />
+        <div className="p-4 bg-gray-100 overflow-y-auto  h-full pl-72 pt-24">
+           
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
