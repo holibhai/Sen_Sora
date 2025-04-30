e397826d8bb4934775e28cada7cbb928d75c8de8
holibhai <146805057+holibhai@users.noreply.github.com>
2025-04-30 15:23:56 +0530

    categry changes

--- my-project/backend/routes/category.routes.js
+++ my-project/backend/routes/category.routes.js
@@ -8,32 +8,13 @@ const {
 } = require("../controller/category.controller");
 
 const router = express.Router();
-const { getCategories } = require("../model/categoryModel")
+// const { getCategories } = require("../model/categoryModel")
 
 
 // Route to create a new category
 router.post("/add", createCategory);
-
-// Route to get all categories
-router.get("/categories", (req, res) => {
-  getCategories((err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
-
-    // Transform flat result to nested structure
-    const grouped = results.reduce((acc, { type, category }) => {
-      if (!acc[type]) acc[type] = [];
-      acc[type].push(category);
-      return acc;
-    }, {});
-    
-    const formatted = Object.entries(grouped).map(([name, subcategories]) => ({
-      name,
-      subcategories
-    }));
-
-    res.json(formatted);
-  });
-});
+ 
+router.get("/getAll", getAllCategories);
 
 // Route to get a category by ID
 router.get("/:id", getCategoryById);
@@ -44,4 +25,4 @@ router.put("/:id", updateCategoryById);
 // Route to delete a category by ID
 router.delete("/:id", deleteCategoryById);
 
-module.exports = router;
+module.exports = router;  
