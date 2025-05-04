99be3e8ee45ea3245a8f0fd82dbe425e366ddd4f
holibhai <146805057+holibhai@users.noreply.github.com>
2025-05-04 10:49:40 +0530

    update date

--- my-project/backend/model/deliveryModel.js
+++ my-project/backend/model/deliveryModel.js
@@ -3,19 +3,19 @@ const { connection } = require("../db/ConnectMysql");
 // Function to create the 'shipping' table
 const createShippingTable = () => {
   const createTableQuery = `
-   CREATE TABLE IF NOT EXISTS shipping (
-  shippingId VARCHAR(8) NOT NULL PRIMARY KEY,
-  firstName VARCHAR(255) NOT NULL,
-  lastName VARCHAR(255) NOT NULL,
-  city VARCHAR(255) NOT NULL,
-  mobileNumber VARCHAR(20) NOT NULL,
-  address1 TEXT NOT NULL,
-  address2 TEXT,
-  orderNotes TEXT,
-  userId VARCHAR(255) NOT NULL,
-  orderId VARCHAR(255) NOT NULL
-);
-
+    CREATE TABLE IF NOT EXISTS shipping (
+      shippingId VARCHAR(8) NOT NULL PRIMARY KEY,
+      firstName VARCHAR(255) NOT NULL,
+      lastName VARCHAR(255) NOT NULL,
+      city VARCHAR(255) NOT NULL,
+      mobileNumber VARCHAR(20) NOT NULL,
+      address1 TEXT NOT NULL,
+      address2 TEXT,
+      orderNotes TEXT,
+      deliveryDate DATETIME DEFAULT NULL, -- Optional delivery date
+      userId VARCHAR(255) NOT NULL,
+      orderId VARCHAR(255) NOT NULL
+    );
   `;
 
   connection.query(createTableQuery, (err, result) => {
