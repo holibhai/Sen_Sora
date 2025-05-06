0ccd09e710a8b214cb1c7ad5d1246b475e074308
holibhai <146805057+holibhai@users.noreply.github.com>
2025-05-06 10:29:32 +0530

    tracker routes

---
+++ my-project/backend/routes/tracker.routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+// Controller functions
+const {
+  signup,
+  login,
+  logout,
+} = require("../controller/tracker.controller");
+
+// @route   POST /api/admin/signup
+// @desc    Register a new admin
+router.post("/signup", signup);
+
+// @route   POST /api/admin/login
+// @desc    Admin login
+router.post("/login", login);
+
+// @route   POST /api/admin/logout
+// @desc    Admin logout
+router.post("/logout", logout);
+
+module.exports = router;
