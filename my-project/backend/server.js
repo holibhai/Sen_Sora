dc1b169600dbf24e3287627e01fb3043c4c4105e
holibhai <146805057+holibhai@users.noreply.github.com>
2025-05-06 08:59:09 +0530

    server.js updated

--- my-project/backend/server.js
+++ my-project/backend/server.js
@@ -26,6 +26,7 @@ const { createDeliveryCostTable } = require("./model/deliveryCostModel");
 const {createOrdersTable} = require("./model/orderModel");
 const { createOrderItemsTable } =require("./model/orderItemModel");
 const {createShippingTable} = require("./model/deliveryModel");
+// const {createAdminsTable} = require("./model/adminModel");
             
 
 dotenv.config();
@@ -37,7 +38,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
 	origin: "http://localhost:3000",  // Replace with your frontend's origin
 	credentials: true                // Allow cookies to be sent
-  }));
+  })); 
 
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -54,6 +55,8 @@ app.use("/api/deliveryCost",deliveryCostRoutes);
 app.use("/api/order",orderRoutes);
 app.use("/api/orderItem",orderItemRoutes);
 app.use("/api/delivery",deliveryRoutes);
+// app.use("/api/admin",deliveryRoutes);
+
 
 
 app.listen(PORT, () => {
