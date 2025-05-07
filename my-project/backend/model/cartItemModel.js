e78a0ce9f3b7df44fb3fbcaaa65db0d232cd236b
holibhai <146805057+holibhai@users.noreply.github.com>
2025-05-07 10:05:15 +0530

    manage cartItems

--- my-project/backend/model/cartItemModel.js
+++ my-project/backend/model/cartItemModel.js
@@ -10,19 +10,20 @@ const createCartItemsTable = () => {
       addedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       price DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
       FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE,
-      FOREIGN KEY (productId) REFERENCES products(id) ON DELETE CASCADE
+      FOREIGN KEY (productId) REFERENCES products(id) ON DELETE CASCADE 
     );
-  `;
-
+  `;                             
+  
   connection.query(createTableQuery, (err, result) => {
-    if (err) {
+    if (err) { 
       console.error("❌ Failed to create cart_items table:", err.message);
     } else {
       console.log("✅ cart_items table created or already exists.");
     }
   });
 };
-
+                                                                                                                       
 module.exports = {
-  createCartItemsTable
-};
+  createCartItemsTable                                                                                     
+};                                                   
+
